f6e02db71a0fd3bb0e8fe4a9eb0340ffb2405870
HAPPY_TELEGRAM <harilalbairwa58@gmail.com>
2025-12-30 12:56:12 +0530

    Update support channel and chat links

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/adityabty/music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN")
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Xmusicsupport")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Xmusicsupport1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GalXyNet")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Besties_Music_Group")
 
 AUTO_LEAVING_ASSISTANT = False
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "3600"))
